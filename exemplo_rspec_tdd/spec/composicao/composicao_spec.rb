2e6da0be8cb7579b41cb65667927c3a33c0dfbaf
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-04-25 14:49:30 -0300

    helper inside specific spec

--- exemplo_rspec_tdd/spec/composicao/composicao_spec.rb
+++ exemplo_rspec_tdd/spec/composicao/composicao_spec.rb
@@ -1,8 +1,14 @@
+require_relative 'helper'
+
+RSpec.configure do |conf|
+  conf.include Helper
+end
+
 describe 'Ruby on Rails' do
   it { is_expected.to start_with("Ruby").and end_with("Rails") }
   it { expect(fruta).to eq('banana').or eq('laranja').or eq('uva')}
 
-  def fruta
-    %w(banana laranja uva).sample
-  end
+  # def fruta # helper method arbitrário
+  #   %w(banana laranja uva).sample
+  # end
 end
