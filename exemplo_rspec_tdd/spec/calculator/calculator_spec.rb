589e064494e63e24536da06d8f996b59bcba0ffd
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-04-25 12:02:55 -0300

    raise error

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,6 +1,19 @@
 require 'calculator'
 
 describe Calculator, ": calculates numbers" do
+  context '#div' do
+    it 'divides by 0' do
+      expect{ subject.div(3, 0) }.to raise_error(ZeroDivisionError)
+      expect{ subject.div(3, 0) }.to raise_error("divided by 0")
+      expect{ subject.div(3, 0) }.to raise_error(ZeroDivisionError, "divided by 0")
+      expect{ subject.div(3, 0) }.to raise_error(/divided/)
+
+      # to test errors you need to use a block,
+      # so it won't stop at the error in calculator
+      # it needs to interpret the error even if it occurs
+    end
+  end
+
   context '#sum' do
     it 'with positive numbers' do
       result = subject.sum(5, 7)
