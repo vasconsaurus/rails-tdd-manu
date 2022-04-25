545e1e1b85f854c58286bdf128bc9aa20d108cb6
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-04-25 14:27:31 -0300

    all

---
+++ exemplo_rspec_tdd/spec/matchers/all/all_spec.rb
@@ -0,0 +1,8 @@
+describe [1,7,9] do
+  it { is_expected.to all (be_odd).and be_an(Integer)}
+end
+
+describe 'all' do
+  it { expect([1,7,9]).to all (be_odd).and be_an(Integer)}
+  it { expect(['ruby', 'rails']).to all(be_a(String).and include('r')) }
+end
