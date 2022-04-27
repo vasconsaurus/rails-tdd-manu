76e36c274da2d8acb9d0b469b9d31a4cdb67ddd7
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-04-27 15:56:39 -0300

    aggregate failures

--- exemplo_rspec_tdd/spec/spec_helper.rb
+++ exemplo_rspec_tdd/spec/spec_helper.rb
@@ -17,6 +17,10 @@ require_relative "../helpers/helper.rb"
 
 RSpec.configure do |config|
 
+  config.define_derived_metadata do |meta|
+    meta[:aggregate_failures] = true
+  end
+
   # config.before(:suite) do
   #   puts '>>>>>>> BEFORE ALL TESTS SUITE'
   # end
