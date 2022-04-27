008a04af64ed7f916e451f888b1a1f584414b071
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-04-27 15:35:02 -0300

    negate matchers

--- exemplo_rspec_tdd/spec/matchers/be_within/be_within_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/be_within/be_within_spec.rb
@@ -1,5 +1,7 @@
+RSpec::Matchers.define_negated_matcher :be_without, :be_within
+
 describe 'be_within' do
   it { expect(12.5).to be_within(0.5).of(12.0)}
   it { expect([11.6, 12.1, 12.4]).to all be_within(0.5).of(12)}
-
+  it { expect(11.4).to be_without(0.5).of(12.0)}
 end
