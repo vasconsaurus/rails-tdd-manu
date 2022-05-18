e24cde21d244951fdf0e7875ee83acfcfd3a16d9
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-05-18 16:52:45 -0300

    custom matchers and tag filters

--- exemplo_rspec_tdd/spec/matchers/all/all_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/all/all_spec.rb
@@ -1,8 +1,13 @@
-describe [1,7,9] do
-  it { is_expected.to all (be_odd).and be_an(Integer)}
-end
+# describe [1,7,9] do
+#   it { is_expected.to all (be_odd).and be_an(Integer)}
+# end
 
-describe 'all' do
+# describe 'all', collection: true do
+# describe 'all', :collection do
+# rodar: rspec . -t collection
+describe 'all', type: 'collection' do
+# rodar: rspec . -t type:collection
+
   it { expect([1,7,9]).to all (be_odd).and be_an(Integer)}
   it { expect(['ruby', 'rails']).to all(be_a(String).and include('r')) }
 end
--- exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
@@ -1,6 +1,11 @@
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe Array.new([1,2,3]) do
+# describe Array.new([1,2,3]), "Array", collection: true do
+# describe Array.new([1,2,3]), "Array", :collection do
+# rodar: rspec . -t collection
+describe Array.new([1,2,3]), "Array", type: 'collection' do
+# rodar: rspec . -t type:collection
+
   it '#include' do
     expect(subject).to include(2)
     expect(subject).to include(2,1)
@@ -8,11 +13,11 @@ describe Array.new([1,2,3]) do
 
   it { expect(subject).to exclude(4) }
 
-  it '#match_array' do
+  it '#match_array', :slow do
     expect(subject).to match_array([1,2,3])
   end
 
-  it '#match_array' do
+  it '#contain_exactly' do
     expect(subject).to contain_exactly(1,2,3)
     expect(subject).to contain_exactly(3,1,2)
   end
