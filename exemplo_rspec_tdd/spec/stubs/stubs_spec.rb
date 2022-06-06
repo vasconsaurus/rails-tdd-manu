2faed62980306f0d369444cf46455d06c4097e7a
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-06 16:02:22 -0300

    method stubs

--- exemplo_rspec_tdd/spec/stubs/stubs_spec.rb
+++ exemplo_rspec_tdd/spec/stubs/stubs_spec.rb
@@ -14,4 +14,37 @@ describe 'Stub' do
 
     expect(finished_course).to be_truthy
   end
+
+  it 'Argumentos Dinâmicos' do
+    student = Student.new
+
+    allow(student).to receive(:foo) do |arg|
+      if arg == :hello
+        "olá"
+      elsif arg == :hi
+        "hi"
+      end
+    end
+
+    expect(student.foo(:hello)).to eq("olá")
+    expect(student.foo(:hi)).to eq("hi")
+  end
+
+  it 'qualquer instância de classe' do
+    student = Student.new
+    other_student = Student.new
+
+    allow_any_instance_of(Student).to receive(:bar).and_return(true)
+
+    expect(student.bar).to be_truthy
+    expect(other_student.bar).to be_truthy
+  end
+
+  it 'erros' do
+    student = Student.new
+
+    allow(student).to receive(:bar).and_raise(RuntimeError)
+
+    expect{ student.bar }.to raise_error(RuntimeError)
+  end
 end
