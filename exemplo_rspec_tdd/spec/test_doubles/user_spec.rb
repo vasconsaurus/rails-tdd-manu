3ffc18bbd94f57d4567b7dc78b29a79ad13312a1
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-06 19:15:51 -0300

    as null object

--- exemplo_rspec_tdd/spec/test_doubles/user_spec.rb
+++ exemplo_rspec_tdd/spec/test_doubles/user_spec.rb
@@ -4,4 +4,21 @@ describe 'Test Double' do
     allow(user_double).to receive_messages(name: 'Jack', password: 'secret')
     user_double.name
   end
+
+  it '---' do
+    user = double('User')
+    allow(user).to receive(:name).and_return('Jack')
+    allow(user).to receive(:password).and_return('secret')
+    puts user.name
+    puts user.password
+  end
+
+  it 'as null object' do
+    user = double('User').as_null_object # dispensa resultados, impede que mensagens de erro sejam levantadas
+    allow(user).to receive(:name).and_return('Jack')
+    allow(user).to receive(:password).and_return('secret')
+    puts user.name
+    puts user.password
+    user.abc
+  end
 end
